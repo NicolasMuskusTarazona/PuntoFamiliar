48520bc4905b0c518bf73656f986e955ef63dc8c
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-01-31 21:57:55 -0600

    feat: :sparkles: Admin Middlewares

--- src/middlewares/admin.middlewares.ts
+++ src/middlewares/admin.middlewares.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const adminOnly = (
-    req: Request,
+    req: Request & { admin?: any },
     res: Response,
     next: NextFunction
 ) => {
@@ -10,15 +10,20 @@ export const adminOnly = (
     if (!auth) {
         return res.status(401).json({ message: "Token requerido" });
     }
+
     const token = auth.split(" ")[1];
+
     try {
         const decoded = jwt.verify(
             token,
             process.env.JWT_SECRET as string
         ) as any;
+
         if (decoded.role !== "admin") {
             return res.status(403).json({ message: "Acceso denegado" });
         }
+
+        req.admin = decoded;
         next();
     } catch {
         return res.status(401).json({ message: "Token invalido" });
