af9e9178bf3b01425b7fb36c4cb6d6a3ebf54e3f
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2026-02-03 23:48:32 -0600

    feat: :sparkles: FRONTEND FOLDER

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from "express"
+import path from "path"
+
 import categoriesRoutes from "./routes/categories.routes"
 import extrasRoutes from "./routes/extras.routes"
 import productsRoutes from "./routes/products.routes"
-import adminRoutes from "./routes/admin.routes";
-
+import adminRoutes from "./routes/admin.routes"
 
 const app = express()
 
 app.use(express.json())
 
+// FRONTEND
+const frontendPath = path.join(__dirname, "..", "frontend")
+app.use(express.static(frontendPath))
+
+// API routes
 app.use("/categories", categoriesRoutes)
 app.use("/extras", extrasRoutes)
 app.use("/products", productsRoutes)
-app.use("/admin", adminRoutes);
-
+app.use("/admin", adminRoutes)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import app from "./app";
 import { testConnection } from "./db";
+import { seedAdmin } from "./seed/createAdmin";
 
 const PORT = 5000;
 
 (async () => {
-    await testConnection(); 
+    await testConnection();
+    await seedAdmin();
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+        console.log(` Server running on port ${PORT}`);
     });
 })();
+
+// OBTENER DATOS DESDE EL CELULAR
+// TENGO QUE ACTUALIZAR EL GET CON UN PUERTO, NO CON UN LOCALHOST LO MISMO EN MENU.JS
+/*(async () => {
+    await testConnection();
+    await seedAdmin();
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log("Servidor corriendo en puerto 5000");
+    });
+
+})();
+*/
